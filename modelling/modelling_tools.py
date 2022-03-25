150493c4806daa4b7cb740b26858476780784ce0
tyst3273 <ty.sterling@colorado.edu>
2022-03-25 10:59:34 -0600

    asdsd

--- modelling/modelling_tools.py
+++ modelling/modelling_tools.py
@@ -9,87 +9,77 @@ import numpy as np
 
 class crystal:
 
-    def __init__(self,basis_pos,basis_types,lattice_vectors,magnetic_moments=None):
+    def __init__(self,atom_pos,atom_types,lattice_vectors,cartesian=False):
 
-        self.lattice_vectors = lattice_vectors
-        self.basis_pos = basis_pos
-        self.basis_types = basis_types
-        self.magnetic_moments = magnetic_moments
-        self.io = interface_io()
+        self.set_lattice_vectors(lattice_vectors)
+        self.set_atom_positions(atom_pos,cartesian)
+        self.set_atom_types(atom_types)
     
     # ----------------------------------------------------------------------------------------------
 
-    @property
-    def lattice_vectors(self):
-        return self._lattice_vectors
-    @lattice_vectors.setter
-    def lattice_vectors(self,value):
+    def set_lattice_vectors(self,lattice_vectors):
+        """
+        lattice_vectors is a 3x3 numpy float array
+        """
         try:
-            value = np.array(value,dtype=float)
+            lattice_vectors = np.array(lattice_vectors,dtype=float)
         except:
-            exit('lattice_vectors should be list of list of floats')
-        if len(value.shape) != 2 or value.shape[0] != 3 or value.shape[1] != 3:
+            exit('lattice_vectors should be list of list of floats, scale should be a float')
+        if len(lattice_vectors.shape) != 2 or lattice_vectors.shape[0] != 3 or \
+                lattice_vectors.shape[1] != 3:
             exit('lattice_vectors should be 3x3 (3D vectors)')
-        #self._num_basis_atoms = value.shape[0]
-        self._lattice_vectors = value
+        self._lattice_vectors = lattice_vectors
 
-    @property
-    def basis_pos(self):
-        return self._basis_pos
-    @basis_pos.setter
-    def basis_pos(self,value):
+    def set_atom_positions(self,atom_pos,cartesian):
+        """
+        atom_pos is an Nx3 numpy float array. cartesian is boolean
+        """
         try:
-            value = np.array(value,dtype=float)
+            atom_pos = np.array(atom_pos,dtype=float)
         except:
-            exit('basis_pos should be list of list of floats')
-        if len(value.shape) != 2 or value.shape[1] != 3:
-            exit('basis_pos should be Nx3 (3D vectors)')
-        self.num_basis_atoms = value.shape[0]
-        self._basis_pos = value
-        self.basis_cart = self._crystal_to_cartesian_coords(self.lattice_vectors,self.basis_pos)
+            exit('atom_pos should be list of list of floats')
+        if len(atom_pos.shape) != 2 or atom_pos.shape[1] != 3:
+            exit('atom_pos should be Nx3 (3D vectors)')
+        self._num_atoms = atom_pos.shape[0]
+        if cartesian:
+            self._cart_pos = atom_pos
+            self._red_pos = self._cartesian_to_crystal_coords(self._lattice_vetors,self._cart_pos)
+        else:
+            self._red_pos = atom_pos
+            self._cart_pos = self._crystal_to_cartesian_coords(self._lattice_vectors,self._red_pos)
 
-    @property
-    def basis_types(self):
-        return self._basis_types
-    @basis_types.setter
-    def basis_types(self,value):
+    def set_atom_types(self,atom_types):
+        """
+        atom_types is an N element list of strings. it's convereted to numpy object array
+        for quick manipulation
+        """
         try:
-            value = np.array(value,dtype=object).flatten()
+            atom_types = np.array(atom_types,dtype=object).flatten()
         except:
-            exit('basis_types should be a list of str')
-        if value.size != self.num_basis_atoms:
-            exit('number of basis_types doesnt match number of positions')
-        self.unique_types, self.type_inds, self.type_counts = np.unique(value,
+            exit('atom_types should be a list of str')
+        if atom_types.size != self._num_atoms:
+            exit('number of atom_types doesnt match number of positions')
+        self._unique_types, self._type_inds, self._type_counts = np.unique(atom_types,
                                     return_inverse=True,return_counts=True)
-        self.num_basis_types = self.unique_types.size
-        self._basis_types = value
-
-    @property
-    def magnetic_moments(self):
-        return self._magnetic_moments
-    @magnetic_moments.setter
-    def magnetic_moments(self,value):
-        self._magnetic_moments = value
+        self._num_types = self._unique_types.size
+        self._atom_types = atom_types
 
     # ----------------------------------------------------------------------------------------------
 
     def _crystal_to_cartesian_coords(self,mat,vecs):
-        """
-        note, need mat in col vectors not row, so transpose
-        """
-        mat = mat.T # need col vectors, not row vectors
-        cart = np.zeros((vecs.shape[0],3))
-        for ii in range(vecs.shape[0]):
-            cart[ii,:] = np.matmul(mat,vecs[ii,:])
-        return cart
+        return self._change_basis(mat,vecs)
 
     def _cartesian_to_crystal_coords(self,mat,vecs):
+        mat = np.linalg.inv(mat)
+        return self._change_basis(mat,vecs) 
+
+    def _change_basis(self,mat,vecs):
         """
-        note, need mat in col vectors not row, so transpose
-        to go from cart. to crystal, use inverse of lattice vectors
+        lattice vectors is matrix of row vectors, but we need column vecs to change basis 
+        correctly. so transpose.
         """
-        mat = np.linalg.inv(mat).T # need col vectors, not row vectors
-        pos = np.zeros((vecs.shape[0],3))
+        mat = mat.T 
+        pos = np.zeros(vecs.shape)
         for ii in range(vecs.shape[0]):
             pos[ii,:] = np.matmul(mat,vecs[ii,:])
         return pos
@@ -101,38 +91,39 @@ class crystal:
         only "diagonal" supercell matrix allowed for now
         """
         try:
-            self.sc_matrix = np.array(sc_matrix,dtype=int)
+            self._sc_matrix = np.array(sc_matrix,dtype=int)
         except:
             exit('sc_matrix must be a list of 3 integers')
 
-        self.num_formula_units = self.sc_matrix.prod()
-        self.num_sc_atoms = self.num_formula_units*self.num_basis_atoms
+        self._num_formula_units = self._sc_matrix.prod()
+        self._num_sc_atoms = self._num_formula_units*self._num_atoms
 
-        self.sc_lattice_vectors = np.zeros((3,3))
+        self._sc_lattice_vectors = np.zeros((3,3))
         for ii in range(3):
-            self.sc_lattice_vectors[ii,:] = self.lattice_vectors[ii,:]*self.sc_matrix[ii]
+            self._sc_lattice_vectors[ii,:] = self._lattice_vectors[ii,:]*self._sc_matrix[ii]
 
-        self.sc_unitcell_inds = np.zeros(self.num_sc_atoms,dtype=int)
-        self.sc_pos = np.zeros((self.num_sc_atoms,3))
-        self.sc_types = np.zeros(self.num_sc_atoms,dtype=object)
+        self._sc_unitcell_inds = np.zeros(self._num_sc_atoms,dtype=int)
+        self._sc_red_pos = np.zeros((self._num_sc_atoms,3))
+        self._sc_types = np.zeros(self._num_sc_atoms,dtype=object)
         shift = 0
         cell = 0
-        for ii in range(self.sc_matrix[0]):
-            for jj in range(self.sc_matrix[1]):
-                for kk in range(self.sc_matrix[2]):
-                    self.sc_pos[shift:shift+self.num_basis_atoms,:] = self.basis_pos[:,:]
-                    self.sc_pos[shift:shift+self.num_basis_atoms,0] += ii
-                    self.sc_pos[shift:shift+self.num_basis_atoms,1] += jj
-                    self.sc_pos[shift:shift+self.num_basis_atoms,2] += kk
-                    self.sc_types[shift:shift+self.num_basis_atoms] = self.basis_types[:]
-                    self.sc_unitcell_inds[shift:shift+self.num_basis_atoms] = cell
+        for ii in range(self._sc_matrix[0]):
+            for jj in range(self._sc_matrix[1]):
+                for kk in range(self._sc_matrix[2]):
+                    self._sc_red_pos[shift:shift+self._num_atoms,:] = self._red_pos[:,:]
+                    self._sc_red_pos[shift:shift+self._num_atoms,0] += ii
+                    self._sc_red_pos[shift:shift+self._num_atoms,1] += jj
+                    self._sc_red_pos[shift:shift+self._num_atoms,2] += kk
+                    self._sc_types[shift:shift+self._num_atoms] = self._atom_types[:]
+                    self._sc_unitcell_inds[shift:shift+self._num_atoms] = cell
                     cell += 1
-                    shift += self.num_basis_atoms
+                    shift += self._num_atoms
 
-        self.sc_pos = self.sc_pos/self.sc_matrix
-        self.sc_unique_types, self.sc_type_inds, self.sc_type_counts = np.unique( 
-                        self.sc_types,return_inverse=True,return_counts=True)
-        self.sc_cart = self._crystal_to_cartesian_coords(self.sc_lattice_vectors,self.sc_pos)
+        self._sc_red_pos = self._sc_red_pos/self._sc_matrix
+        self._sc_unique_types, self._sc_type_inds, self._sc_type_counts = np.unique( 
+                        self._sc_types,return_inverse=True,return_counts=True)
+        self._sc_cart_pos = self._crystal_to_cartesian_coords(self._sc_lattice_vectors,
+                        self._sc_red_pos)
 
     # ----------------------------------------------------------------------------------------------
 
@@ -143,8 +134,8 @@ class crystal:
         according to Euler angles. Euler angles are in degrees. See goldstein sec 4.4
         for conventions
         """
-        if not hasattr(self,'sc_pos'):
-            exit('no sc_pos. did you use make_supercell?')
+        if not hasattr(self,'_sc_red_pos'):
+            exit('no _sc_red_pos. did you use make_supercell?')
         try:
             euler_angles = np.array(euler_angles,dtype=float).flatten()
         except:
@@ -168,11 +159,12 @@ class crystal:
         R_full = np.matmul(R_psi,np.matmul(R_theta,R_phi))
 
         # find nn
-        rel, shift = self._get_minimum_image_arrays(coord,self.sc_pos)
+        rel, shift = self._get_minimum_image_arrays(coord,self._sc_red_pos)
         min_im_pos = rel+shift
-        min_im_cart = self._crystal_to_cartesian_coords(self.sc_lattice_vectors,min_im_pos)
+        min_im_cart = self._crystal_to_cartesian_coords(self._sc_lattice_vectors,min_im_pos)
         min_im_dist = np.sqrt((min_im_cart**2).sum(axis=1))
-        nn_inds = np.argwhere(min_im_dist <= nn_dist).flatten()
+        self._which_to_rotate = (min_im_dist <= nn_dist)
+        nn_inds = np.argwhere(self._which_to_rotate).flatten()
 
         # apply the rotation
         for ii in range(nn_inds.size):
@@ -180,12 +172,12 @@ class crystal:
             min_im_cart[ind,:] = np.matmul(R_full,min_im_cart[ind,:])
 
         # now put pos back into crystal coords and unshift minimum image
-        min_im_pos = self._cartesian_to_crystal_coords(self.sc_lattice_vectors,min_im_cart)
+        min_im_pos = self._cartesian_to_crystal_coords(self._sc_lattice_vectors,min_im_cart)
         min_im_pos = min_im_pos+coord-shift
 
         # replace sc positions with new ones
-        self.sc_cart = self._crystal_to_cartesian_coords(self.sc_lattice_vectors,min_im_pos)
-        self.sc_pos = min_im_pos
+        self._sc_cart_pos = self._crystal_to_cartesian_coords(self._sc_lattice_vectors,min_im_pos)
+        self._sc_red_pos = min_im_pos
 
     # ----------------------------------------------------------------------------------------------
 
@@ -196,21 +188,6 @@ class crystal:
 
     # ----------------------------------------------------------------------------------------------
 
-    def write_poscar(self,out_file='POSCAR',which='primitive'):
-        self.io.write_poscar(self,out_file,which)
-
-    def write_xyz(self,out_file='pos.xyz',which='primitive'):
-        self.io.write_xyz(self,out_file,which)
-
-    def write_cp2k(self,out_file='cp2k.pos',which='primitive'):
-        self.io.write_cp2k(self,out_file,which)
-
-    def write_abivars(self,out_file='abivars',which='primitive'):
-        self.io.write_abivars(self,out_file,which)
-
-    # ----------------------------------------------------------------------------------------------
-
-
 # --------------------------------------------------------------------------------------------------
 
 class interface_io:
@@ -219,21 +196,11 @@ class interface_io:
     read and write from DFT calculators
     """
 
-    def write_abivars(self,crystal,out_file,which):
+    def write_abivars(self,crystal,out_file='abivars',which='primitive'):
         if which == 'primitive':
-            pos = crystal.basis_pos
-            lat_vecs = crystal.lattice_vectors
-            types = crystal.unique_types
-            counts = crystal.type_counts
-            inds = crystal.type_inds
+            pos, lat_vecs, types, counts, inds = self._get_prim(crystal)
         elif which == 'supercell':
-            if not hasattr(crystal,'sc_pos'):
-                exit('no sc_pos. did you use make_supercell?')
-            pos = crystal.sc_pos
-            lat_vecs = crystal.sc_lattice_vectors
-            types = crystal.sc_unique_types
-            counts = crystal.sc_type_counts
-            inds = crystal.sc_type_inds
+            pos, lat_vecs, types, counts, inds = self._get_sc(crystal)
 
         sort = np.argsort(inds).flatten()
         inds = inds[sort]
@@ -254,21 +221,11 @@ class interface_io:
                 f_out.write(f' {pos[ii,0]: 12.9f} {pos[ii,1]: 12.9f}' \
                             f' {pos[ii,2]: 12.9f} {types[inds[ii]]}\n')
 
-    def write_poscar(self,crystal,out_file,which):
+    def write_poscar(self,crystal,out_file='POSCAR',which='primitive'):
         if which == 'primitive':
-            pos = crystal.basis_pos
-            lat_vecs = crystal.lattice_vectors
-            types = crystal.unique_types
-            counts = crystal.type_counts
-            inds = crystal.type_inds
+            pos, lat_vecs, types, counts, inds = self._get_prim(crystal)
         elif which == 'supercell':
-            if not hasattr(crystal,'sc_pos'):
-                exit('no sc_pos. did you use make_supercell?')
-            pos = crystal.sc_pos
-            lat_vecs = crystal.sc_lattice_vectors
-            types = crystal.sc_unique_types
-            counts = crystal.sc_type_counts
-            inds = crystal.sc_type_inds
+            pos, lat_vecs, types, counts, inds = self._get_sc(crystal)
 
         sort = np.argsort(inds).flatten()
         inds = inds[sort]
@@ -281,11 +238,11 @@ class interface_io:
                 f_out.write(f' {lat_vecs[ii,0]: 12.9f} {lat_vecs[ii,1]: 12.9f}'\
                              f' {lat_vecs[ii,2]: 12.9f}\n')            
             _ = ''
-            for ii in range(crystal.num_basis_types):
+            for ii in range(crystal._num_types):
                 _ += f' {types[ii]}'
             f_out.write(_)
             _ = '\n '
-            for ii in range(crystal.num_basis_types):
+            for ii in range(crystal._num_types):
                 _ += f' {counts[ii]}'
             f_out.write(_)
             f_out.write('\nDirect\n')
@@ -293,21 +250,11 @@ class interface_io:
                 f_out.write(f' {pos[ii,0]: 12.9f} {pos[ii,1]: 12.9f}' \
                             f' {pos[ii,2]: 12.9f} {types[inds[ii]]}\n')
 
-    def write_xyz(self,crystal,out_file,which):
+    def write_xyz(self,crystal,out_file='pos.xyz',which='primitive'):
         if which == 'primitive':
-            cart = crystal.basis_cart
-            lat_vecs = crystal.lattice_vectors
-            types = crystal.unique_types
-            counts = crystal.type_counts
-            inds = crystal.type_inds
+            cart, lat_vecs, types, counts, inds = self._get_prim(crystal,cartesian=True)
         elif which == 'supercell':
-            if not hasattr(crystal,'sc_pos'):
-                exit('no sc_pos. did you use make_supercell?')
-            cart = crystal.sc_cart
-            lat_vecs = crystal.sc_lattice_vectors
-            types = crystal.sc_unique_types
-            counts = crystal.sc_type_counts
-            inds = crystal.sc_type_inds
+            cart, lat_vecs, types, counts, inds = self._get_sc(crystal,cartesian=True)
 
         sort = np.argsort(inds).flatten()
         inds = inds[sort]
@@ -320,27 +267,25 @@ class interface_io:
                 f_out.write(f' {types[inds[ii]]} {cart[ii,0]: 12.9f} {cart[ii,1]: 12.9f}' \
                             f' {cart[ii,2]: 12.9f}\n')
 
-    def write_cp2k(self,crystal,out_file,which):
+    def write_cp2k(self,crystal,out_file='cp2k.pos',which='primitive'):
         if which == 'primitive':
-            pos = crystal.basis_pos
-            lat_vecs = crystal.lattice_vectors
-            types = crystal.unique_types
-            counts = crystal.type_counts
-            inds = crystal.type_inds
+            pos, lat_vecs, types, counts, inds = self._get_prim(crystal)
         elif which == 'supercell':
-            if not hasattr(crystal,'sc_pos'):
-                exit('no sc_pos. did you use make_supercell?')
-            pos = crystal.sc_pos
-            lat_vecs = crystal.sc_lattice_vectors
-            types = crystal.sc_unique_types
-            counts = crystal.sc_type_counts
-            inds = crystal.sc_type_inds
+            pos, lat_vecs, types, counts, inds = self._get_sc(crystal)
 
         sort = np.argsort(inds).flatten()
         inds = inds[sort]
         pos = pos[sort,:]
         num_atoms = pos.shape[0]
         tab = ' '*4
+
+        if hasattr(crystal,'_which_to_rotate'):
+            rot = np.argwhere(crystal._which_to_rotate[sort]).flatten()
+            _ = 'the following atoms have been \'rotated\':\n\t'
+            for ii in range(rot.size):
+                _ = _+f'{rot[ii]+1:g} '
+            print(_)
+
         with open(out_file,'w') as f_out:
             f_out.write(2*tab+f'&CELL\n')
             f_out.write(3*tab+f'A {lat_vecs[0,0]: 12.9f} {lat_vecs[0,1]: 12.9f}'\
@@ -357,6 +302,30 @@ class interface_io:
                 f_out.write(3*tab+f'{types[inds[ii]]:<2} {pos[ii,0]: 12.9f}'\
                                 f' {pos[ii,1]: 12.9f} {pos[ii,2]: 12.9f}\n')
             f_out.write(2*tab+f'&END COORD')
+
+    def _get_prim(self,crystal,cartesian=False):
+        if cartesian:
+            pos = crystal._cart_pos
+        else:
+            pos = crystal._red_pos
+        lat_vecs = crystal._lattice_vectors
+        types = crystal._unique_types
+        counts = crystal._type_counts
+        inds = crystal._type_inds
+        return pos, lat_vecs, types, counts, inds
+
+    def _get_sc(self,crystal,cartesian=False):
+        if not hasattr(crystal,'_sc_red_pos'):
+            exit('no _sc_red_pos. did you use make_supercell?')
+        if cartesian:
+            pos = crystal._sc_cart_pos
+        else:
+            pos = crystal._sc_red_pos
+        lat_vecs = crystal._sc_lattice_vectors
+        types = crystal._sc_unique_types
+        counts = crystal._sc_type_counts
+        inds = crystal._sc_type_inds
+        return pos, lat_vecs, types, counts, inds
 
     # ----------------------------------------------------------------------------------------------
 
